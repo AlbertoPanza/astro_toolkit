dfd106e4d7d5de1e4841c731cd59e711bf7e6bdb
AlbertoPanza <albertopanza@hotmail.it>
2025-04-14 12:49:09 +0200

    Update main.py
    integrated bodies.json

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import json
 from astro import gravitational_force
 
-# Example: Earth and Moon
-mass_earth = 5.972e24 # in kg
-mass_moon = 7.348e22 # in kg
+# Load the JSON file
+with open("bodies.json", "r") as file:
+  bodies = json.load(file)
+
+mass_earth = bodies["earth"]
+mass_moon = bodies["moon"]
 distance = 384_400_000 # in meters
 
 force = gravitational_force(mass_earth, mass_moon, distance)
